c83df70b100ac0e0efda3afbe0bcdff1a1e22ca7
unknown <yosamelody07@gmail.com>
2020-09-08 05:15:33 +0800

    change port

--- app.js
+++ app.js
@@ -79,6 +79,11 @@ app
     });
   });
 
-app.listen(3000, function () {
-  console.log("Server run on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+  console.log("this port is 3000");
 });
